b1cb0424e621a5ac34694c7f4cc21fbfc07e1415
Andrea Bevoni <andreabevoni@gmail.com>
2020-12-02 15:17:02 +0100

    Fine milestone 1

---
+++ js/script.js
@@ -0,0 +1,34 @@
+// In questo esercizio iniziamo a replicare la logica che sta dietro a tantissimi siti che permettono la visione di film e telefilm.
+// Per fare questo, come fanno siti molto più rinomati, utilizzeremo un API che ci permette di avere un insieme di risultati congrui alla nostra ricerca.
+
+// Milestone 1:
+// Creare un layout base con una searchbar (una input e un button) in cui possiamo scrivere completamente o parzialmente il nome di un film.
+// Possiamo, cliccando il bottone, cercare sull’API tutti i film che contengono ciò che ha scritto l’utente.
+// Vogliamo dopo la risposta dell’API visualizzare a schermo i seguenti valori per ogni film trovato:
+// Titolo, Titolo Originale, Lingua e Voto
+
+const database = "https://api.themoviedb.org/3/search/movie?api_key=1b9a718974945696fe81f966f55c9ee4&language=it-IT";
+
+var app = new Vue({
+  el: "#root",
+  data: {
+    search: "",
+    show: false,
+    dbMovie: []
+  },
+  methods: {
+    // funzione per cercare sull'API i film
+    startSearch: function() {
+      // alla pressione del bottone controllo se l'utente ha inserito una stringa per effettuare la ricerca
+      if (this.search != "") {
+        this.show = true;
+        axios.get(`${database}&query=${this.search}`)
+        .then(result => {
+          this.dbMovie = result.data.results;
+        });
+      } else {
+        this.show = false;
+      }
+    }
+  }
+});
